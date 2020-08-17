e288e5a2be6947902e651e88dbdd370b73af27fb
Sarangan0219 <sarangan@wso2.com>
2020-08-17 10:55:54 +0530

    Refine error logs in archive import

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OASParserUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OASParserUtil.java
@@ -102,6 +102,7 @@ import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -710,6 +711,9 @@ public class OASParserUtil {
         File[] listOfFiles = new File(extractedLocation).listFiles();
         File archiveDirectory = null;
         if (listOfFiles != null) {
+            if (listOfFiles.length > 1) {
+                throw new APIManagementException("Swagger Definitions should be placed under one root folder.");
+            }
             for (File file: listOfFiles) {
                 if (file.isDirectory()) {
                     archiveDirectory = file.getAbsoluteFile();
@@ -717,7 +721,8 @@ public class OASParserUtil {
                 }
             }
         }
-        //verify whether the zipped input is archive or file.
+        //Verify whether the zipped input is archive or file.
+        //If it is a single  swagger file without remote references it can be imported directly, without zipping.
         if (archiveDirectory == null) {
             throw new APIManagementException("Could not find an archive in the given ZIP file.");
         }
